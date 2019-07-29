238b884a97a1bd8eec1c38025aff2d1a61f1d979
Alex Lepsa <lepsalex@gmail.com>
2019-07-29 11:25:36 -0400

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SDEGithubIntegration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
