3bf3fa687af9d518cd244602433dfe46054dc4e3
Alex <lepsalex@gmail.com>
2019-08-08 11:32:55 -0400

    Multi-threading working, needs cleanup

--- sdk/SDEClient.cs
+++ sdk/SDEClient.cs
@@ -13,6 +13,7 @@ namespace SDEIntegration
     {
         ConsumerConfig consumerConfig;
         IConsumer<Ignore, sdk.proto.Task> taskConsumer;
+        IConsumer<Ignore, sdk.proto.TaskNote> taskNoteConsumer;
         IIntegrationClient<Task<SDEIssue>, Task<SDENote>> integrationClient;
 
         private const string CREATE_TOPIC_NAME = "task-new";
@@ -25,44 +26,75 @@ namespace SDEIntegration
 
             var serviceUrl = Environment.GetEnvironmentVariable("SERVICE_URL");
 
-            consumerConfig = new ConsumerConfig {
-              GroupId = "github-integration",
-              BootstrapServers = serviceUrl != null ? serviceUrl : "localhost:9092",
-              AutoOffsetReset = AutoOffsetReset.Earliest
+            consumerConfig = new ConsumerConfig
+            {
+                GroupId = "github-integration",
+                BootstrapServers = serviceUrl != null ? serviceUrl : "localhost:9092",
+                AutoOffsetReset = AutoOffsetReset.Earliest
             };
 
             taskConsumer = new ConsumerBuilder<Ignore, sdk.proto.Task>(consumerConfig)
                             .SetValueDeserializer(new ProtobufDeserializer<sdk.proto.Task>())
                             .Build();
-            taskConsumer.Subscribe(new List<string>(){CREATE_TOPIC_NAME, UPDATE_TOPIC_NAME});
+
+            taskNoteConsumer = new ConsumerBuilder<Ignore, sdk.proto.TaskNote>(consumerConfig)
+                            .SetValueDeserializer(new ProtobufDeserializer<sdk.proto.TaskNote>())
+                            .Build();
+
+            taskConsumer.Subscribe(new List<string>() { CREATE_TOPIC_NAME, UPDATE_TOPIC_NAME });
+            taskNoteConsumer.Subscribe(ADD_NOTE_TOPIC_NAME);
         }
 
         public void run()
         {
-            Log.Information("SDE Client Start ...");
+            Log.Information("SDE Client Start!");
 
             CancellationTokenSource cts = new CancellationTokenSource();
-            Console.CancelKeyPress += (_, e) => {
+            Console.CancelKeyPress += (_, e) =>
+            {
                 e.Cancel = true; // prevent the process from terminating.
                 cts.Cancel();
             };
 
+            var consumers = new System.Threading.Tasks.Task[] { startTaskConsumer(cts), startTaskNoteConsumer(cts) };
+
             try
+            {
+                System.Threading.Tasks.Task.WaitAll(consumers, cts.Token);
+            }
+            catch (OperationCanceledException)
+            {
+                // Ensure the consumers leaves the group cleanly and final offsets are committed.
+                taskConsumer.Close();
+                taskNoteConsumer.Close();
+                Log.Information("All Consumers closed!");
+            }
+
+        }
+
+        private System.Threading.Tasks.Task startTaskConsumer(CancellationTokenSource cts)
+        {
+            Log.Information("Task Consumer listening ...");
+
+            return new System.Threading.Tasks.Task(() =>
             {
                 while (true)
                 {
+                    Log.Information("Waiting for Task message ...");
+
                     try
                     {
                         var taskMsg = taskConsumer.Consume(cts.Token);
 
-                        if (taskMsg.Topic.Equals(CREATE_TOPIC_NAME)) {
+                        if (taskMsg.Topic.Equals(CREATE_TOPIC_NAME))
+                        {
                             integrationClient.CreateIssue(taskMsg.Value);
-                        } else if (taskMsg.Topic.Equals(UPDATE_TOPIC_NAME)) {
-                            integrationClient.UpdateIssue(taskMsg.Value);
-                        } else if (taskMsg.Topic.Equals(ADD_NOTE_TOPIC_NAME)) {
-                            integrationClient.CreateIssueNote(taskMsg.Value);
                         }
-                        
+                        else if (taskMsg.Topic.Equals(UPDATE_TOPIC_NAME))
+                        {
+                            integrationClient.UpdateIssue(taskMsg.Value);
+                        }
+
                         Log.Information($"Consumed message '{taskMsg.Value}' at: '{taskMsg.TopicPartitionOffset}'.");
                     }
                     catch (ConsumeException e)
@@ -70,16 +102,31 @@ namespace SDEIntegration
                         Log.Error($"Error occurred: {e.Error.Reason}");
                     }
                 }
-            }
-            catch (OperationCanceledException)
-            {
-                // Ensure the consumer leaves the group cleanly and final offsets are committed.
-                taskConsumer.Close();
-            }
+            });
         }
 
-        private System.Threading.Tasks.Task startConsumer(List<string> topics, ProtobufDeserializer<?> deserializer) {
+        private System.Threading.Tasks.Task startTaskNoteConsumer(CancellationTokenSource cts)
+        {
+            Log.Information("Task Note Consumer listening ...");
 
+            return new System.Threading.Tasks.Task(() =>
+            {
+                while (true)
+                {
+                    Log.Information("Waiting for Task Note message ...");
+
+                    try
+                    {
+                        var taskMsg = taskNoteConsumer.Consume(cts.Token);
+                        integrationClient.CreateIssueNote(taskMsg.Value);
+                        Log.Information($"Consumed message '{taskMsg.Value}' at: '{taskMsg.TopicPartitionOffset}'.");
+                    }
+                    catch (ConsumeException e)
+                    {
+                        Log.Error($"Error occurred: {e.Error.Reason}");
+                    }
+                }
+            });
         }
     }
 }
